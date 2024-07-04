22b009186ee68171d7f689ac9a1189ece243e916
HoangVHSE184022 <145216389+HoangVHSE184022@users.noreply.github.com>
2024-07-04 12:29:30 +0700

    fix

--- src/main/java/com/project/DASBackend/config/FirebaseConfig.java
+++ src/main/java/com/project/DASBackend/config/FirebaseConfig.java
@@ -6,42 +6,27 @@ import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
-//    @Bean
-//    public FirebaseApp initializeFirebase() throws IOException {
-//        // Load the service account key JSON file from the classpath
-//        try (InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream("serviceAccountKey.json")) {
-//            if (serviceAccount == null) {
-//                throw new FileNotFoundException("serviceAccountKey.json not found in classpath");
-//            }
-//
-//            // Initialize Firebase options with the service account credentials
-//            FirebaseOptions options = new FirebaseOptions.Builder()
-//                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-//                    .build();
-//
-//            // Initialize and return the FirebaseApp instance
-//            return FirebaseApp.initializeApp(options);
-//        }
-//    }
-
     @Bean
-    public FirebaseApp firebaseApp() throws IOException {
-        FileInputStream serviceAccount =
-                new FileInputStream("path/to/serviceAccountKey.json");
+    public FirebaseApp initializeFirebase() throws IOException {
+        // Load the service account key JSON file from the classpath
+        try (InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream("serviceAccountKey.json")) {
+            if (serviceAccount == null) {
+                throw new IOException("serviceAccountKey.json not found in classpath");
+            }
 
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build();
+            // Initialize Firebase options with the service account credentials
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
 
-        return FirebaseApp.initializeApp(options);
+            // Initialize and return the FirebaseApp instance
+            return FirebaseApp.initializeApp(options);
+        }
     }
-
 }
